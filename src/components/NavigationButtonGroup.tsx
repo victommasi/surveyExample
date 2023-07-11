4335636905f2575f8f788a3851cb5e166934a319
Victor Tommasi <victor.tommasi@inductivehealth.com>
2023-07-11 10:34:39 -0300

    fix - fixing next button bg color

--- src/components/NavigationButtonGroup.tsx
+++ src/components/NavigationButtonGroup.tsx
@@ -15,15 +15,15 @@ const NavigationButtonGroup = (props: NavigationButtonGroupProps) => {
       <button
         type="button"
         onClick={props.handlePrevious}
-        className="outline-0 px-4 py-2 w-24 text-sm font-medium text-gray-800 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-violet-700 focus:z-10 focus:ring-2 focus:ring-violet-700 focus:text-violet-700">
+        className="outline-0 px-4 py-2 w-24 text-sm font-medium text-gray-800 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 focus:z-10 focus:ring-2 focus:ring-violet-700">
         Previous
       </button>
       <button
         type="submit"
         disabled={!isValid}
-        className={classNames("outline-0 transition-colors disabled:opacity-50 px-4 py-2 w-24 text-sm font-medium text-gray-800 bg-white border border-gray-200 rounded-r-lg focus:z-10 focus:ring-2 ", {
-          "text-gray-300 hover:bg-gray-100 hover:text-violet-700": !isValid,
-          "bg-violet-600 text-white !hover:bg-violet-100 !hover:text-white focus:ring-violet-700 focus:text-white": isValid
+        className={classNames("outline-0 transition-colors disabled:opacity-50 px-4 py-2 w-24 text-sm font-medium text-gray-800 border border-gray-200 rounded-r-lg focus:z-10 focus:ring-2 ", {
+          "bg-white text-gray-300 hover:bg-gray-100": !isValid,
+          "bg-violet-600 text-white focus:ring-violet-700 focus:text-white": isValid
         })}>
         Next
       </button>
